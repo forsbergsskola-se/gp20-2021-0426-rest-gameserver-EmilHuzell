252236df45db34c2ca43617e7296802e86a344fe
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-04-28 12:03:50 +0200

    adds getrequest to Acme.com

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TinyBrowser
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            
+            
+            var timeServer = new TcpClient("Acme.com", 80);
+            Console.WriteLine("Waiting for connection to establish");
+            var stream = timeServer.GetStream();
+            var send = "Get http://www.Acme.com HTTP/0.9\r\n\r\n";
+            stream.Write(Encoding.ASCII.GetBytes(send,0,send.Length));
+            var sr = new StreamReader(stream);
+            var str = sr.ReadToEnd();
+            Console.WriteLine(str);
+            timeServer.Close();
+            stream.Close();
+                
+            
         }
     }
 }
