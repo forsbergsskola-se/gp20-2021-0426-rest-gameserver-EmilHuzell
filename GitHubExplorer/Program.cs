d2b8e96386fe90d2623ac79ba2eaabbf005b940a
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-05-21 17:14:56 +0200

    Fixes bugs on githubExplorer

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -8,18 +10,26 @@ namespace GitHubExplorer
     public class UserResponse {
         public string name{get;set;} 
         public string job {get; set;}
+        public string location {get; set;}
+        public string company { get; set;}
+        public string organizations_url { get; set;}
     }
+
+    
     class Program
     {
-        public static string token = "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";
+        private static string token = "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";
         static async Task Main(string[] args) {
-            
+            //e
             var client = new HttpClient();
             
             client.DefaultRequestHeaders.Add("Authorization", $"Token {token}");
             client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+
+            
             while(true)
             {
+                Dictionary<string, string> user = new Dictionary<string, string>();
                 Console.WriteLine("Submit a username you'd like to explore");
             
                 string username = Console.ReadLine();
@@ -30,17 +40,30 @@ namespace GitHubExplorer
 
                 var response = client.Send(request);
                 string contents =  await response.Content.ReadAsStringAsync();
+                
+                //Console.WriteLine(contents);
 
                 var profile = JsonSerializer.Deserialize<UserResponse>(contents);
-                Console.WriteLine(profile.job + profile.name);
-            }
-            
-            
-           
-            //Console.WriteLine(response.ToString());
-            
-        }
+                PropertyInfo[] props = Type.GetType("GitHubExplorer.UserResponse").GetProperties();
+                
 
-        
+                foreach (PropertyInfo property in props) {
+                    if (property.GetValue(profile) == null) {
+                        continue;
+                    }
+                   
+                    Console.WriteLine($"{property.Name} {property.GetValue(profile)}");
+                    Console.WriteLine();
+                    
+                }
+                //cd Documents\GitHub\gp20-2021-0426-rest-gameserver-EmilHuzell\GitHubExplorer
+                
+                //Console.WriteLine(String.Empty);
+                //Console.WriteLine("Name: " + user["location"]);
+                //Console.WriteLine("Location: " + profile.location);
+                //Console.WriteLine($"{profile.organizations_url}");
+                
+            }
+        }
     }
 }
